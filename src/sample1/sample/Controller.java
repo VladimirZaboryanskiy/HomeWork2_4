a456be7502e49b7179cf5237edab6cb74dc83c7d
VladimirZaboryanskiy <Sentinel2003@mail.ru>
2020-11-19 21:12:38 +0400

    HOME24

---
+++ src/sample1/sample/Controller.java
@@ -0,0 +1,32 @@
+package sample1.sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    public TextArea textArea;
+
+
+    public void onClickSend(ActionEvent actionEvent) {
+        textArea.appendText("");
+
+
+    }
+
+    public void clickClose(ActionEvent actionEvent) {
+
+    }
+
+    public void clickNumber(ActionEvent actionEvent) {
+        textArea.appendText(((Button)actionEvent.getSource()).getText());
+    }
+
+    public void clickOperation(ActionEvent actionEvent) {
+        textArea.appendText(((Button)actionEvent.getSource()).getText());
+    }
+}

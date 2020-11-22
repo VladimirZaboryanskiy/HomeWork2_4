b2d7979895adbbabc5a10b4761975274494d51cc
VladimirZaboryanskiy <Sentinel2003@mail.ru>
2020-11-22 21:50:03 +0400

    HOME24

--- src/sample1/sample/Controller.java
+++ src/sample1/sample/Controller.java
@@ -1,10 +1,9 @@
-package sample1.sample;
+package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 public class Controller {
 //
